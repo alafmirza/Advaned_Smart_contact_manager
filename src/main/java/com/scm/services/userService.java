7770f25ec2cbe73ada091d1ee2f8cd80b69c7feb
Alaf Baig <mirzaalaf21@gmai.com>
2025-10-12 23:19:56 +0530

    user Serviuce interface

--- src/main/java/com/scm/services/userService.java
+++ src/main/java/com/scm/services/userService.java
@@ -1,8 +1,35 @@
 package com.scm.services;
 
+import java.lang.foreign.Linker.Option;
+import java.util.List;
+import java.util.Optional;
+
+import com.scm.entities.User;
 
 public interface userService {
     
+   //save 
+   User saveUser(User user);
 
+   //find by id
+   Optional<User> singleUser(String id);
 
+   //find all
+   List<User> allUsers();
+
+   //find by email
+   User emailUser(String email);
+
+   //update
+   Optional<User> updateUser(User user);
+
+   //delete
+  void deleteUser(String id);
+
+   //userexist by id
+   boolean isUserExist(String id);
+
+   //userexist by emai
+   boolean isUserExistByemail(String email);
+ 
 }
