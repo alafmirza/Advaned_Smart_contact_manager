ca834488bdf09839166e10526599869db87acfd8
Alaf Baig <mirzaalaf21@gmai.com>
2025-11-19 18:12:56 +0530

    security

--- src/main/java/com/scm/serviceImplement/service.java
+++ src/main/java/com/scm/serviceImplement/service.java
@@ -7,7 +7,6 @@ import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import com.scm.dao.userRepository;
